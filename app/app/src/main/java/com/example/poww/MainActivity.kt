b922a856a4ae0f9f8729de6456709e654a1b4584
RedisTheOne <redis123445@gmail.com>
2021-01-30 22:48:07 +0100

    aa

--- app/app/src/main/java/com/example/poww/MainActivity.kt
+++ app/app/src/main/java/com/example/poww/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.poww
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+    private val LOGIN_ACTIVITY_CODE: Int = 1
+    private lateinit var logInLinearLayout: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logInLinearLayout = findViewById(R.id.login_in_linear_layout)
+        logInLinearLayout.setOnClickListener{
+            val intent: Intent = Intent(this, LoginActivity::class.java)
+            startActivityForResult(intent, LOGIN_ACTIVITY_CODE)
+        }
+        supportActionBar?.hide()
     }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const router = express.Router()
 const con = require('../database')
+const jwt = require('jsonwebtoken')
+const jwtKey = require('../config').jwtKey
+
+//GET USER INFORMATION
+router.get('/', (req, res) => {
+    if(req.headers['authorization']) {
+        const token = req.headers['authorization'].split("Bearer ")[1]
+        jwt.verify(token, jwtKey, async (err, decoded) => {
+            if(err)
+                res.json({
+                    status: false,
+                    msg: 'Token is not valid'
+                })
+            else {
+                res.json({
+                    status: true,
+                    user: decoded.user
+                })
+            }
+        })
+    }
+})
 
 //LOGIN
 router.post('/login', (req, res) => {
     if(req.body.username && req.body.password) {
         con.query(`SELECT * FROM Users WHERE username = '${req.body.username}' AND password = '${req.body.password}';`, (err, result, fields) => {
-            if(result.length > 0) 
-                res.json({
-                    status: true,
-                    msg: 'Logged in successfully'
+            if(result.length > 0) {
+                jwt.sign({user: result[0]}, jwtKey, {expiresIn: '30d'}, (err, token) => {
+                    res.json({
+                        status: true,
+                        msg: 'Logged in successfully',
+                        jwt: token
+                    })
                 })
+            }
             else 
                 res.json({
                     status: false,
@@ -37,9 +63,14 @@ router.post('/signup', (req, res) => {
                 const sql = `INSERT INTO Users(username, email, password) 
                 VALUES('${req.body.username}', '${req.body.email}', '${req.body.password}')`
                 con.query(sql, (err, result) => {
-                    res.json({
-                        status: true,
-                        msg: 'User created successfully'
+                    con.query(`SELECT * FROM Users WHERE username = '${req.body.username}' AND password = '${req.body.password}'`, (err, result, fields) => {
+                        jwt.sign({user: result[0]}, jwtKey, {expiresIn: '30d'}, (err, token) => {
+                            res.json({
+                                status: true,
+                                msg: 'User created successfully',
+                                jwt: token
+                            })
+                        })
                     })
                 })
             }
